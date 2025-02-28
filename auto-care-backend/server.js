beae8abfdb416d9c305f74548ab5192af46e5866
Vaibhavi <vaibhavichaudhari304@gmail.com>
2025-02-28 09:24:37 +0530

    updated

--- auto-care-backend/server.js
+++ auto-care-backend/server.js
@@ -21,8 +21,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-// app.use(cors());
-// const cors = require("cors");
+
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 app.use(helmet());
